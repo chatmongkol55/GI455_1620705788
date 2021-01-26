b0197328911db4a89be64fc75e6f2b29ff4e2307
Chatmongkol <chatmongkol.phon@bumail.net>
2021-01-26 15:17:00 +0700

    Send Assignment Week1

---
+++ GI455_1620705788/Assets/Find.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Find : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string input;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    void Start()
+    {
+        
+    }
+    void update()
+    {
+        input = inputField.GetComponent<Text>().text;
+
+        switch(input)
+        {
+            case "Almond pretzel":
+                textDisplay.GetComponent<Text>().text = input + " is found";
+                break;
+
+            case "Bacon":
+                textDisplay.GetComponent<Text>().text = input + " is found"; 
+                break;
+
+            case "Chocolate":
+                textDisplay.GetComponent<Text>().text = input + " is found";
+                break;
+
+            case "Latte":
+                textDisplay.GetComponent<Text>().text = input + " is found";
+                break;
+
+            case "Oreo frappe":
+                textDisplay.GetComponent<Text>().text = input + " is found";
+                break;
+
+            default:
+                textDisplay.GetComponent<Text>().text =" Not found";
+                break;
+
+        }   
+        
+    }
+}
